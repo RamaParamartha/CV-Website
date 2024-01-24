8cf7bcd86401000b6b12515601960f6103d4f989
Pyro <paramartha1307@gmail.com>
2024-01-24 15:13:54 +0800

    new features

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
+"use client"
 import React from 'react';
 import './globals.css'
+import { TypeAnimation } from 'react-type-animation';
 
 const HomePage = () => {
   return (
-    <div className="m-24">
-    <p className=" text-4xl">Hello this is a typewriter</p>
+    
+    
+    <div className="mx-24">
+    <TypeAnimation
+  sequence={[
+    'We got unggas',
+    1000,
+    'We got strong unggas',
+    1000,
+    'We got half asian unggas',
+    1000,
+    'We got premium unggas',
+    1000,
+    'All for 50% off',
+    10000,
+  ]}
+  speed={50}
+  style={{ fontSize: '2em' }}
+  repeat={Infinity}
+  />
     </div>
     
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,22 +9,22 @@ const Navbar = () => {
     <nav className="bg-gray-900 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-            <p className='text-xl animate-pulse'>paramartha.cv</p>
+            <p className='text-xl animate-pulse'>swiftsurge.cv</p>
         </div>
         <div>
           <ul className="flex space-x-4">
             <li>
               <Link href="/">
-                <button className="text-white padding-up">Home</button>
+                <button className="text-white padding-up transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Home</button>
               </Link>
             </li>
             <li>
               <Link href="/about">
-                <button className="text-white padding-up">About</button>
+                <button className="text-white padding-up transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">About</button>
               </Link>
             </li>
             <li>
-              <button className="text-white border-solid border-2 border-sky-500 transition-colors hover:bg-sky-500 rounded px-2">
+              <button className="text-white border-solid border-2 border-blue-500 transition-colors hover:bg-sky-500 rounded px-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 hover:border-indigo-500">
                 <a href="https://drive.google.com/uc?export=download&id=1m5vbUBu3N17mwc095yGkOnL_-ddrasY_">Download PDF</a>
               </button>
             </li>
