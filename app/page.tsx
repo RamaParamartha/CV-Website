1ee479537f2dc76cd8c3efe1da400d34e8a1315b
Pyro <paramartha1307@gmail.com>
2024-01-29 09:48:50 +0800

    Improved Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,37 @@
-"use client"
+"use client";
 import React from 'react';
 import './globals.css';
 import { TypeAnimation } from 'react-type-animation';
+import Image from 'next/image';
+import Balloon from '@/components/ease-in-animation';
+
 
 const HomePage = () => {
   return (
-    <div className="top-60">
-      <TypeAnimation className='text-blue-800'
-        sequence={[
-          'BLAaauegggrh',
-          1000,
-          'satria baja hitam',
-          1000,
-          'bebek hitam',
-          1000,
-          'its your lover koe ngapain ngomong gitu ke orang ram rawr',
-          1000,
-          'aaaaa mau ram lesgo ram',
-          1000,
-        ]}
-        speed={50}
-        style={{ fontSize: '4em' }}
-        repeat={Infinity}
-      />
-      <p className='text-9xl' id='section1'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora amet, velit nesciunt quod, repellat natus ratione possimus fugiat unde mollitia numquam iusto atque optio perferendis iste magnam voluptatem. Reprehenderit, corrupti!
-        <a href="#section2" className='text-blue-500'>click me</a>
-      </p>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p className='text-9xl' id='section2'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora amet, velit nesciunt quod, repellat natus ratione possimus fugiat unde mollitia numquam iusto atque optio perferendis iste magnam voluptatem. Reprehenderit, corrupti!
-        <a href="#section1" className='text-blue-500'>click me</a>
-      </p>
+  <div className='h-[100rem]'>
+    <div className="bg-white rounded overflow-hidden w-[30rem] h-[20rem] m-auto p-4">
+        <h2 className=" text-2xl font-bold mb-2 text-black">Card title</h2>
+        <TypeAnimation className='text-black overflow-hidden'
+      sequence={[
+        
+        3000,
+        'We produce food for People',
+        100,
+        'We produce food for Animals',
+        100,
+        'We produce food for Trees',
+        100,
+        'We produce food for Buildings and Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+        1000
+      ]}
+      wrapper="span"
+      speed={70}
+      style={{ fontSize: '1em', display: 'inline-block', width: '28rem', height: '14rem' }}
+      repeat={Infinity}
+    />
+        <a href="#" className="text-black hover:underline ">Card link</a>
+        <a href="#" className="text-black hover:underline ml-2">Another link</a>
+      </div>
     </div>
   );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 p-4 sticky top-0">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="bg-gray-900 p-4 sticky top-0 z-10 shadow-md shadow-black">
+      <div className="container mx-auto flex justify-between items-center sticky">
         <div>
             <p className='text-xl animate-pulse'>swiftsurge.cv</p>
         </div>
@@ -14,16 +14,16 @@ const Navbar = () => {
           <ul className="flex space-x-4">
             <li>
               <Link href="#section1">
-                <button className="text-white padding-up transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Home</button>
+                <button className="text-white padding-up transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 scroll-smooth">Home</button>
               </Link>
             </li>
             <li>
               <Link href="#section2">
-                <button className="text-white padding-up transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">About</button>
+                <button className="text-white padding-up transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 scroll-smooth">About</button>
               </Link>
             </li>
             <li>
-              <button className="text-white border-solid border-2 border-blue-500 rounded px-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-white duration-300 hover:border-white hover:text-black">
+              <button className="text-white border-solid border-2 shadow-lg shadow-blue-500/50 border-blue-500 rounded px-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-white duration-300 hover:border-white hover:text-black">
                 <a href="https://drive.google.com/uc?export=download&id=1m5vbUBu3N17mwc095yGkOnL_-ddrasY_">Download CV</a>
               </button>
             </li>
