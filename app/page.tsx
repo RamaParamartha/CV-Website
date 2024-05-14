db300f1b70dc94b7d169ff1bf214cbcd6b7eb614
Pyro <paramartha1307@gmail.com>
2024-05-15 01:12:35 +0800

    new updet

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ const HomePage = () => {
       <Profile />
       <div className="h-[70rem] spacer layer2" id="report">
         <p className="text-white font-sans text-center text-8xl font-bold pt-4">REPORT</p>
-        <div className="ml-72 ">
-            <p>test</p>
-            <img src="taksu_tech.svg" alt="logo perusahaan" className="bg-white p-4 rounded scale-[200%]"/>
+        <div className="ml-96 mt-60">
+            <p className="text-2xl font-bold font-sans ml-96">ayoniga?</p>
+            <img src="taksu_tech.svg" alt="logo perusahaan" className="bg-white p-4 rounded shadow-xl shadow-black duration-300 hover:shadow-blue-500 hover:bg-white animate-upNdown3-1"/>
         </div>
       </div>
     </div>
